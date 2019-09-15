37e8b392adbe56d6e5c49f84d2b91f77a225831a
Carlos Guimaraes <sekmet@open4g.com>
2019-09-15 17:43:44 -0300

    updates!

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,14 +6,26 @@
 
 // You can delete this file if you're not using it
 const path = require('path')
+const TerserJSPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
-exports.onCreateWebpackConfig = ({ actions }) => {
+exports.onCreateWebpackConfig = ({ stage, actions, plugins }) => {
+
+    const isProduction = stage !== 'develop';
+
+    if (isProduction) {
+
     actions.setWebpackConfig({
         /*externals: {
             $: 'jquery',
             jQuery: 'jquery',
             'window.jQuery': 'jquery'
         },*/
+        devtool: false,
+        optimization: {
+            minimizer: [new OptimizeCSSAssetsPlugin({})],
+        },
         resolve: {
             alias: {
                 components: path.resolve(__dirname, 'src/components'),
@@ -22,4 +34,19 @@ exports.onCreateWebpackConfig = ({ actions }) => {
             },
         },
     })
+
+    } else {
+
+        actions.setWebpackConfig({
+            resolve: {
+                alias: {
+                    components: path.resolve(__dirname, 'src/components'),
+                    templates: path.resolve(__dirname, 'src/templates'),
+                    //scss: path.resolve(__dirname, 'src/sass'),
+                },
+            },
+        })
+
+    }
+
 }
